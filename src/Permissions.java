98c050d24c193b6d75cf217542c46f8b1ff85701
Robert <mcfaddenbones@gmail.com>
2026-03-16 18:31:59 -0700

    Delete src directory

--- src/Permissions.java
+++
@@ -1,7 +0,0 @@
-public interface Permissions 
-{
-    public boolean hasPermission(String user);
-    public void    addPermission(String user);
-    public void    removePermission(String user);
-    public void    showPermissions();
-}
