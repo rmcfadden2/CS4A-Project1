2cb4e3bd0388a2bea87ffd9fffb361e59da34e6d
Masonman101 <165975990+Masonman101@users.noreply.github.com>
2026-03-25 07:37:50 -0700

    Update ComfortDevice.java
    Updated member functions

--- ComfortDevice.java
+++ ComfortDevice.java
@@ -1,18 +1,39 @@
 public class ComfortDevice extends SmartHome {
 
     private int temperatureSetting;
+    private int rpm;
+    private int channel;
 
-    public ComfortDevice(String name, String color, int yearMade, int temperatureSetting) {
+    public ComfortDevice(String name, String color, int yearMade, int temperatureSetting, int rpm, int channel) {
         super(name, color, yearMade);
         this.temperatureSetting = temperatureSetting;
+        this.rpm = rpm;
+        this.channel = channel;
     }
 
     public int getTemperatureSetting() {
         return temperatureSetting;
     }
 
+        public int getRpm() {
+        return rpm;
+    }
+
+    public int getChannel() {
+        return channel;
+    }
+
+
     public void setTemperatureSetting(int temperatureSetting) {
         this.temperatureSetting = temperatureSetting;
+    }
+
+    public void setRpm(int rpm) {
+        this.rpm = rpm;
+    }
+
+    public void setChannel(int channel) {
+        this.channel = channel;
     }
 
     @Override
@@ -28,6 +49,8 @@ public class ComfortDevice extends SmartHome {
     @Override
     public String toString() {
         return super.toString() + 
-               "Temperature Setting: " + temperatureSetting + "\n";
+               "Temperature Setting: " + temperatureSetting + " F\n" +
+               "Temperature Setting: " + rpm + " rpm\n" +
+               "TV Channel: " + channel + "\n";
     }
 }
