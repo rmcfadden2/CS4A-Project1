c3b9ab99cc6d814992dbf06c9fc5616c949d9728
Masonman101 <165975990+Masonman101@users.noreply.github.com>
2026-03-25 07:37:02 -0700

    Update UtilityDevice.java
    Updated with new member functions

--- UtilityDevice.java
+++ UtilityDevice.java
@@ -1,19 +1,42 @@
 public class UtilityDevice extends SmartHome {
 
     private int powerUsage;
+    private int brightness;
+    private boolean isClean;
 
-    public UtilityDevice(String name, String color, int yearMade, int powerUsage) {
+    public UtilityDevice(String name, String color, int yearMade, int powerUsage, int brightness, boolean isClean) {
         super(name, color, yearMade);
         this.powerUsage = powerUsage;
+        this.brightness = brightness;
+        this.isClean = isClean;
+        
     }
 
     public int getPowerUsage() {
         return powerUsage;
     }
 
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public boolean getIsClean() {
+        return isClean;
+    }
+
+
     public void setPowerUsage(int powerUsage) {
         this.powerUsage = powerUsage;
     }
+
+    public void setBrightness(int brightness) {
+        this.brightness = brightness;
+    }
+
+    public void setIsClean(boolean isClean) {
+        this.isClean = isClean;
+    }
+
 
     @Override
     public boolean isOn() {
@@ -28,6 +51,8 @@ public class UtilityDevice extends SmartHome {
     @Override
     public String toString() {
         return super.toString() + 
-               "Power Usage: " + powerUsage + " watts\n";
+               "Power Usage: " + powerUsage + " watts\n" +
+               "Brightness: " + brightness + " lumens\n" +
+               "Cleaning Status: " + (isClean ? "Cleaned" : "Not Cleaned");
     }
 }
